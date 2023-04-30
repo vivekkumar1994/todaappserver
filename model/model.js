80e181f68da332be7f90d22e03f53f11c8ea338c
vivekkumar1994 <96063498+vivekkumar1994@users.noreply.github.com>
2023-04-30 17:26:41 +0530

    first commit

---
+++ model/model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+const TodoSchema = new mongoose.Schema({
+    data: {
+        type: String,
+        required: true
+    },
+    dates:{
+        type: String,
+        required: true
+        
+    },
+    done: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const todo = mongoose.model('todo', TodoSchema);
+
+export default todo;
